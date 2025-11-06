a6b3e25a7968a661c84f143f2f699e81144c5b61
Abhinav Kumar Yadav <abhinavk1608@gmail.com>
2025-11-06 17:20:34 +0530

    Added Comment controller , routes and model

--- Backend-Project/src/app.js
+++ Backend-Project/src/app.js
@@ -17,11 +17,17 @@ app.use(cookieParser())
 
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 //Router declaration
                                     //standard practice for url kuch hai nhi ye api/v1
 app.use("/api/v1/user",userRouter) // jaise hi url me /api/v1/user ayega to controle router ke paas jayega aur fir routing wahi se hogi.
                                   //  we can use directly router.get("/register",userregister) but for modularity and big projects we prefer above syntax.
 
-app.use("/api/v1/video",videoRouter)                                  
+app.use("/api/v1/video",videoRouter) 
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/comment", commentRouter);
+
+
 export {app}
